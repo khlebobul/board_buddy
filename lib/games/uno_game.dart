99c32113111c4bba4b0bb30898ea5b5681e3ff3c
khlebobul <sbgleb10@gmail.com>
2025-01-11 17:47:27 +0300

    add game card icons for Uno-like game

--- lib/games/uno_game.dart
+++ lib/games/uno_game.dart
@@ -11,73 +11,7 @@ class UnoGame extends StatelessWidget {
         onLeftButtonPressed: () => Navigator.pop(context),
         rightButtonText: S.of(context).uno,
         // for testing
-        onRightButtonPressed: () {
-          showModalBottomSheet(
-            context: context,
-            builder: (context) => const AddPlayersBottomSheet(),
-          );
-        },
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: CustomKeyboard(
-          buttons: [
-            [
-              KeyboardButton(
-                buttonText: '1',
-                onPressed: () {},
-              ),
-              KeyboardButton(
-                buttonText: '2',
-                onPressed: () {},
-              ),
-              KeyboardButton(
-                buttonText: '3',
-                onPressed: () {},
-              ),
-              KeyboardButton(
-                buttonText: '4',
-                onPressed: () {},
-              ),
-            ],
-            [
-              KeyboardButton(
-                buttonText: '5',
-                onPressed: () {},
-              ),
-              KeyboardButton(
-                buttonText: '6',
-                onPressed: () {},
-              ),
-              KeyboardButton(
-                buttonText: '7',
-                onPressed: () {},
-              ),
-              KeyboardButton(
-                buttonText: '8',
-                onPressed: () {},
-              ),
-            ],
-            [
-              KeyboardButton(
-                buttonText: '9',
-                onPressed: () {},
-              ),
-              KeyboardButton(
-                buttonText: '0',
-                onPressed: () {},
-              ),
-              KeyboardButton(
-                buttonText: 'x',
-                onPressed: () {},
-              ),
-              KeyboardButton(
-                buttonText: 'y',
-                onPressed: () {},
-              ),
-            ],
-          ],
-        ),
+        onRightButtonPressed: () {},
       ),
     );
   }
--- lib/utils/custom_icons.dart
+++ lib/utils/custom_icons.dart
@@ -5,4 +5,16 @@ class CustomIcons {
   static const String refresh = 'assets/icons/rotate-ccw.svg';
   static const String edit = 'assets/icons/edit.svg';
   static const String remove = 'assets/icons/remove.svg';
+
+  // game cards
+  // uno, dos, uno flip
+  static const String reverse = 'assets/icons/game_cards/uno_like/reverse.svg';
+  static const String skip = 'assets/icons/game_cards/uno_like/skip.svg';
+  static const String swap = 'assets/icons/game_cards/uno_like/swap_hands.svg';
+  static const String wild = 'assets/icons/game_cards/uno_like/wild.svg';
+  static const String wildDrawFour = 'assets/icons/game_cards/uno_like/wild_4.svg';
+  static const String wildDrawTwoDos = 'assets/icons/game_cards/uno_like/wild_2.svg';
+  static const String flip = 'assets/icons/game_cards/uno_like/flip.svg';
+  static const String skipEveryone = 'assets/icons/game_cards/uno_like/skip_everyone.svg';
+  static const String wildDrawColor = 'assets/icons/game_cards/uno_like/wild_draw_color.svg';
 }
