fde209ad4c8127e0bc8419665ebe4a2f35a17656
khlebobul <sbgleb10@gmail.com>
2025-01-23 08:22:55 +0300

    add munchkin icons

--- lib/utils/custom_icons.dart
+++ lib/utils/custom_icons.dart
@@ -8,15 +8,30 @@ class CustomIcons {
   static const String leftArrow = 'assets/icons/left-arrow.svg';
   static const String rightArrow = 'assets/icons/right-arrow.svg';
 
-  // game cards
+  // games icons
   // uno, dos, uno flip
-  static const String reverse = 'assets/icons/game_cards/uno_like/reverse.svg';
-  static const String skip = 'assets/icons/game_cards/uno_like/skip.svg';
-  static const String swap = 'assets/icons/game_cards/uno_like/swap_hands.svg';
-  static const String wild = 'assets/icons/game_cards/uno_like/wild.svg';
-  static const String wildDrawFour = 'assets/icons/game_cards/uno_like/wild_4.svg';
-  static const String wildDrawTwoDos = 'assets/icons/game_cards/uno_like/wild_2.svg';
-  static const String flip = 'assets/icons/game_cards/uno_like/flip.svg';
-  static const String skipEveryone = 'assets/icons/game_cards/uno_like/skip_everyone.svg';
-  static const String wildDrawColor = 'assets/icons/game_cards/uno_like/wild_draw_color.svg';
+  static const String reverse = 'assets/icons/games_icons/uno_like/reverse.svg';
+  static const String skip = 'assets/icons/games_icons/uno_like/skip.svg';
+  static const String swap = 'assets/icons/games_icons/uno_like/swap_hands.svg';
+  static const String wild = 'assets/icons/games_icons/uno_like/wild.svg';
+  static const String wildDrawFour =
+      'assets/icons/games_icons/uno_like/wild_4.svg';
+  static const String wildDrawTwoDos =
+      'assets/icons/games_icons/uno_like/wild_2.svg';
+  static const String flip = 'assets/icons/game_cards/games_icons/flip.svg';
+  static const String skipEveryone =
+      'assets/icons/games_icons/uno_like/skip_everyone.svg';
+  static const String wildDrawColor =
+      'assets/icons/games_icons/uno_like/wild_draw_color.svg';
+
+  // munchkin
+  static const String flask = 'assets/icons/games_icons/munchkin/flask.svg';
+  static const String hat = 'assets/icons/games_icons/munchkin/hat.svg';
+  static const String shield = 'assets/icons/games_icons/munchkin/shield.svg';
+  static const String sparkles =
+      'assets/icons/games_icons/munchkin/sparkles.svg';
+  static const String sword = 'assets/icons/games_icons/munchkin/sword.svg';
+  static const String swords = 'assets/icons/games_icons/munchkin/swords.svg';
+  static const String male = 'assets/icons/games_icons/munchkin/male.svg';
+  static const String female = 'assets/icons/games_icons/munchkin/female.svg';
 }
