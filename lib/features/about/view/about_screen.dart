afb3b3e0f8fa20c658b047373126400513e00820
khlebobul <sbgleb10@gmail.com>
2025-04-13 13:44:39 +0300

    add blur effect fot about screen

--- lib/features/about/view/about_screen.dart
+++ lib/features/about/view/about_screen.dart
@@ -8,6 +8,7 @@ import 'package:board_buddy/features/about/widgets/contributors_widget.dart';
 import 'package:board_buddy/shared/widgets/ui/custom_app_bar.dart';
 import 'package:board_buddy/features/about/widgets/link_btn.dart';
 import 'package:board_buddy/shared/services/review_service.dart';
+import 'package:board_buddy/shared/widgets/ui/blurred_scroll_view.dart';
 import 'package:flutter/material.dart';
 
 class AboutScreen extends StatefulWidget {
@@ -41,124 +42,115 @@ class _AboutScreenState extends State<AboutScreen> {
         onRightButtonPressed: () {},
       ),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-                    horizontal: GeneralConst.paddingHorizontal) +
-                const EdgeInsets.only(top: GeneralConst.paddingVertical),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text.rich(
+        child: BlurredScrollView(
+          maskColor: theme.bgColor,
+          padding: const EdgeInsets.symmetric(
+                  horizontal: GeneralConst.paddingHorizontal) +
+              const EdgeInsets.only(top: GeneralConst.paddingVertical),
+          children: [
+            Text.rich(
+              TextSpan(
+                children: [
                   TextSpan(
-                    children: [
-                      TextSpan(
-                        text: GeneralConst.appName,
-                        style: theme.display2.copyWith(
-                          color: theme.redColor,
-                        ),
-                      ),
-                      TextSpan(
-                        text: S
-                            .of(context)
-                            .letsYouTrackScoresAndKeyMomentsEffortlesslyKeepingYour,
-                        style: theme.display2.copyWith(
-                          color: theme.secondaryTextColor,
-                        ),
-                      ),
-                    ],
+                    text: GeneralConst.appName,
+                    style: theme.display2.copyWith(
+                      color: theme.redColor,
+                    ),
                   ),
-                  softWrap: true,
-                  overflow: TextOverflow.clip,
-                ),
-                const SizedBox(height: 10),
-                LinkBtnWidget(
-                  text: S.of(context).rateTheApp,
-                  url: Platform.isIOS
-                      ? AppLnksConst.rateAppStore
-                      : AppLnksConst.rateGooglePlay,
-                ),
-                LinkBtnWidget(
-                  text: S.of(context).shareWithFriedns,
-                  shareText:
-                      '${S.of(context).boardBuddyIsYourUltimateBoardGameCompanion}\n\n$urlPreview',
-                  url: AppLnksConst.webSitelink,
-                  isShareButton: true,
-                ),
-                LinkBtnWidget(
-                  text: S.of(context).projectWebsite,
-                  url: AppLnksConst.webSitelink,
-                ),
-                LinkBtnWidget(
-                  text: S.of(context).telegram,
-                  url: AppLnksConst.telegramLink,
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  S.of(context).sinceThisIsAnOpenSourceProjectYouCanLeave,
-                  style:
-                      theme.display2.copyWith(color: theme.secondaryTextColor),
-                ),
-                LinkBtnWidget(
-                  text: S.of(context).githubRepository,
-                  url: AppLnksConst.githubLink,
-                ),
-                const SizedBox(height: 10),
-                const AddFavouriteGame(),
-                const SizedBox(height: 10),
-                LinkBtnWidget(
-                  text: S.of(context).followMeOnXTwitter,
-                  url: AppLnksConst.xLink,
-                ),
-                LinkBtnWidget(
-                  text: S.of(context).checkMyWebsite,
-                  url: AppLnksConst.myWebSitelink,
-                ),
-                const SizedBox(height: 10),
-                Divider(
-                  color: theme.borderColor,
-                  thickness: 0.7,
-                ),
-                LinkBtnWidget(
-                  text: S.of(context).takeALookAtMyKnightsGraphApp,
-                  url: Platform.isIOS
-                      ? AppLnksConst.kgAppStore
-                      : AppLnksConst.kgGooglePlay,
-                ),
-                Divider(
-                  color: theme.borderColor,
-                  thickness: 0.7,
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  S.of(context).appreciation,
-                  style:
-                      theme.display2.copyWith(color: theme.secondaryTextColor),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  S.of(context).toAllWhoCreatedThe,
-                  style:
-                      theme.display2.copyWith(color: theme.secondaryTextColor),
-                ),
-                const LinkBtnWidget(
-                  text: GeneralConst.lucideIcons,
-                  url: AppLnksConst.lucideIconsLink,
-                ),
-                const SizedBox(height: 15),
-                Text(
-                  S.of(context).toBoardBuddysContributors,
-                  style:
-                      theme.display2.copyWith(color: theme.secondaryTextColor),
-                ),
-                const ContributorsWidget(
-                  contributors: ContributorsConst.contributors,
-                ),
-                const SizedBox(height: 50),
-              ],
+                  TextSpan(
+                    text: S
+                        .of(context)
+                        .letsYouTrackScoresAndKeyMomentsEffortlesslyKeepingYour,
+                    style: theme.display2.copyWith(
+                      color: theme.secondaryTextColor,
+                    ),
+                  ),
+                ],
+              ),
+              softWrap: true,
+              overflow: TextOverflow.clip,
             ),
-          ),
+            const SizedBox(height: 10),
+            LinkBtnWidget(
+              text: S.of(context).rateTheApp,
+              url: Platform.isIOS
+                  ? AppLnksConst.rateAppStore
+                  : AppLnksConst.rateGooglePlay,
+            ),
+            LinkBtnWidget(
+              text: S.of(context).shareWithFriedns,
+              shareText:
+                  '${S.of(context).boardBuddyIsYourUltimateBoardGameCompanion}\n\n$urlPreview',
+              url: AppLnksConst.webSitelink,
+              isShareButton: true,
+            ),
+            LinkBtnWidget(
+              text: S.of(context).projectWebsite,
+              url: AppLnksConst.webSitelink,
+            ),
+            LinkBtnWidget(
+              text: S.of(context).telegram,
+              url: AppLnksConst.telegramLink,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              S.of(context).sinceThisIsAnOpenSourceProjectYouCanLeave,
+              style: theme.display2.copyWith(color: theme.secondaryTextColor),
+            ),
+            LinkBtnWidget(
+              text: S.of(context).githubRepository,
+              url: AppLnksConst.githubLink,
+            ),
+            const SizedBox(height: 10),
+            const AddFavouriteGame(),
+            const SizedBox(height: 10),
+            LinkBtnWidget(
+              text: S.of(context).followMeOnXTwitter,
+              url: AppLnksConst.xLink,
+            ),
+            LinkBtnWidget(
+              text: S.of(context).checkMyWebsite,
+              url: AppLnksConst.myWebSitelink,
+            ),
+            const SizedBox(height: 10),
+            Divider(
+              color: theme.borderColor,
+              thickness: 0.7,
+            ),
+            LinkBtnWidget(
+              text: S.of(context).takeALookAtMyKnightsGraphApp,
+              url: Platform.isIOS
+                  ? AppLnksConst.kgAppStore
+                  : AppLnksConst.kgGooglePlay,
+            ),
+            Divider(
+              color: theme.borderColor,
+              thickness: 0.7,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              S.of(context).appreciation,
+              style: theme.display2.copyWith(color: theme.secondaryTextColor),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              S.of(context).toAllWhoCreatedThe,
+              style: theme.display2.copyWith(color: theme.secondaryTextColor),
+            ),
+            const LinkBtnWidget(
+              text: GeneralConst.lucideIcons,
+              url: AppLnksConst.lucideIconsLink,
+            ),
+            const SizedBox(height: 15),
+            Text(
+              S.of(context).toBoardBuddysContributors,
+              style: theme.display2.copyWith(color: theme.secondaryTextColor),
+            ),
+            const ContributorsWidget(
+              contributors: ContributorsConst.contributors,
+            ),
+            const SizedBox(height: 50),
+          ],
         ),
       ),
     );
