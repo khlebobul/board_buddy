63403bc025126eedf0adb57d7a03dab5dda662d1
khlebobul <sbgleb10@gmail.com>
2025-04-16 09:08:43 +0300

    fix MunchkinGame icons colors

--- lib/features/games/muncknin/view/munchkin_game.dart
+++ lib/features/games/muncknin/view/munchkin_game.dart
@@ -121,6 +121,8 @@ class _MunchkinGameState extends State<MunchkinGame> {
                               state.players[_currentPlayerIndex].isMale
                                   ? CustomIcons.male
                                   : CustomIcons.female,
+                              colorFilter: ColorFilter.mode(
+                                  theme.textColor, BlendMode.srcIn),
                             ),
                           ),
                           const SizedBox(width: 40),
@@ -163,7 +165,11 @@ class _MunchkinGameState extends State<MunchkinGame> {
                                     .add(ResetPlayerModifiers(currentPlayer));
                               }
                             },
-                            child: SvgPicture.asset(CustomIcons.bone),
+                            child: SvgPicture.asset(
+                              CustomIcons.bone,
+                              colorFilter: ColorFilter.mode(
+                                  theme.textColor, BlendMode.srcIn),
+                            ),
                           ),
                         ],
                       ),
