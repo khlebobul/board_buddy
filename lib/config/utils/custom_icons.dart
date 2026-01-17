1ceaa4e1f06e85e46acafaae0a438bedefa0318a
Gleb Shalimov <sbgleb10@gmail.com>
2026-01-17 09:54:51 +0300

    add animated icons for BottomGameBar

--- lib/config/utils/custom_icons.dart
+++ lib/config/utils/custom_icons.dart
@@ -5,12 +5,8 @@ class CustomIcons {
   static const String refresh = 'assets/icons/rotate-ccw.svg';
   static const String edit = 'assets/icons/edit.svg';
   static const String remove = 'assets/icons/remove.svg';
-  static const String leftArrow = 'assets/icons/left-arrow.svg';
-  static const String rightArrow = 'assets/icons/right-arrow.svg';
-  static const String info = 'assets/icons/info.svg';
   static const String sun = 'assets/icons/sun.svg';
   static const String sunDark = 'assets/icons/sun-dark.svg';
-  static const String keyboard = 'assets/icons/keyboard.svg';
   static const String warning = 'assets/icons/warning.svg';
 
   // games icons
